3df3843e77c719121f2aef1aa9b70420e096b4d8
toniraga <eragatony@gmail.com>
2021-05-19 23:38:55 +0100

    payment card and other components completed

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const Input = ({ label, classNames, containerClass, value, onChange }) => {
+const Input = ({
+	label,
+	type,
+	classNames,
+	containerClass,
+	value,
+	onChange,
+	placeholder,
+	...props
+}) => {
 	return (
 		<div className={`card_group ${containerClass}`}>
 			<label className="dynamic_label"> {label} </label>
 			<input
+				{...props}
 				value={value}
+				type={type}
 				onChange={onChange}
+				placeholder={placeholder}
 				className={`dynamic_input ${classNames}`}
 			/>
 		</div>
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import paypal from '../img/paypal1.png';
+import visa from '../img/visa.jpg';
+
 import Input from './Input';
+import PaymentCard from 'react-payment-card-component';
 
 const Payment = () => {
+	const [cvvFocused, setCvvFocused] = useState(false);
+	const [cardNumber, setCardNumber] = useState('1728 3849 4904 0427');
+	const [cvv, setCvv] = useState('234');
+	const [expDate, setExpDate] = useState('12/30');
+
+	const onFocus = () => setCvvFocused(true);
+	const onBlur = () => setCvvFocused(false);
+
 	return (
 		<div className="payment">
 			<div className="payment_method">
@@ -9,38 +21,83 @@ const Payment = () => {
 					<span className="payment_cap"> Payment Information </span>
 					<span style={{ opacity: '0.4' }}>Choose your method of payment.</span>
 				</p>
-				<p>
-					<span> Visa Img </span>
-					<span> other Img </span>
+				<p style={{ display: 'flex', alignItems: 'center' }}>
 					<span>
-						<span>select</span> Paypal Img
+						<img
+							style={{ width: '30px', height: '20px', marginRight: '10px' }}
+							src={visa}
+							alt=""
+						/>
+					</span>
+					<span>
+						<img
+							style={{ width: '50px', height: '30px', marginRight: '10px' }}
+							src={paypal}
+							alt=""
+						/>
+					</span>
+					<span style={{ display: 'flex', alignItems: 'center' }}>
+						<span>
+							<input type="radio" />
+						</span>
+						<img
+							style={{ width: '50px', height: '30px' }}
+							src={paypal}
+							alt=""
+						/>
 					</span>
 				</p>
 			</div>
 			<div className="card_container">
 				<div className="card_atm">
-					<div className="card">
-						<p> CARD NUMBER </p>
-						<p>4 3 2 4 5 4 3 3 9 3 8 2 1 0 3 0</p>
-					</div>
+					<PaymentCard
+						bank="santander"
+						model="prime"
+						type="color"
+						className="card"
+						brand="mastercard"
+						number={cardNumber}
+						cvv={cvv}
+						holderName="Toni Raga"
+						expiration={expDate}
+						flipped={cvvFocused}
+					/>
 				</div>
 				<div className="card_inputs">
 					<div className="card_input">
 						<div style={{ width: '46%' }}>
-							<Input value={'yemm'} label="Credit card number" />
+							<Input
+								value={cardNumber}
+								type="number"
+								placeholder="1728 3849 4904 0427"
+								onChange={(e) => setCardNumber(e.target.value)}
+								label="Credit card number"
+							/>
 							<Input
 								containerClass="card_group_addon"
-								value={'yemm'}
+								type="number"
+								placeholder="324"
+								onFocus={onFocus}
+								onBlur={onBlur}
+								value={cvv}
+								onChange={(e) => setCvv(e.target.value)}
 								label="Security Code"
 							/>
 						</div>
 
 						<div style={{ width: '46%' }}>
-							<Input value={'yemm'} label="Expiration date" />
 							<Input
-								value={'yemm'}
+								value={expDate}
+								type="text"
+								placeholder="12/30"
+								label="Expiration date"
+								onChange={(e) => setExpDate(e.target.value)}
+							/>
+							<Input
 								containerClass="card_group_addon"
+								placeholder="10119"
 								label="Postal Code"
+								type="number"
 							/>
 						</div>
 					</div>
